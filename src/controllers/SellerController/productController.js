662b9f9da5631d6a0aeae7b9cf77f39869731b97
Ashish Brahambhatt <ashishbrahambhatt95@gmail.com>
2025-05-09 14:26:29 +0530

    Update productController.js

--- src/controllers/SellerController/productController.js
+++ src/controllers/SellerController/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/SellerPanelModule/productModel");
 const { S3Client, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const Seller = require("../../models/SellerPanelModule/sellerModel");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const s3 = new S3Client({
